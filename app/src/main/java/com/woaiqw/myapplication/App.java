59ff5d02e3c2f3754338f3f7bb1ff97ef23d3924
woaigmz <woaigmz@gmail.com>
2018-09-17 11:09:09 +0800

    create mp

---
+++ app/src/main/java/com/woaiqw/myapplication/App.java
@@ -0,0 +1,14 @@
+package com.woaiqw.myapplication;
+
+import android.app.Application;
+
+/**
+ * Created by haoran on 2018/9/12.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
