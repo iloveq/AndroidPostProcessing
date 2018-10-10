bd02a1cf5ced8defc3f977a687014c2c4b4fa5ea
woaigmz <woaigmz@gmail.com>
2018-10-10 15:28:27 +0800

    logger

--- postprocessing-compiler/src/main/java/com/woaiqw/appcompiler/utils/Constants.java
+++ postprocessing-compiler/src/main/java/com/woaiqw/appcompiler/utils/Constants.java
@@ -4,5 +4,9 @@ package com.woaiqw.appcompiler.utils;
  * Created by haoran on 2018/10/10.
  */
 public class Constants {
+
+    // Log
+    static final String PREFIX_OF_LOGGER = "AndroidPostProcessingLogger";
+
     public static final String ANNOTATION_ACTION_PATH = "com.woaiqw.postprocessingannotation.App";
 }
