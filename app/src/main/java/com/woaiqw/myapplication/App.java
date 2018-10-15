c887e4a2edf041894545e167b1530679424bbb16
woaigmz <woaigmz@gmail.com>
2018-10-15 10:50:20 +0800

    rm -f hugo temp

--- app/src/main/java/com/woaiqw/myapplication/App.java
+++ app/src/main/java/com/woaiqw/myapplication/App.java
@@ -4,14 +4,14 @@ import android.app.Application;
 
 import com.woaiqw.postprocessing.AndroidPostProcessing;
 
-import hugo.weaving.DebugLog;
+//import hugo.weaving.DebugLog;
 
 /**
  * Created by haoran on 2018/9/12.
  */
 public class App extends Application {
 
-    @DebugLog
+    //@DebugLog
     @Override
     public void onCreate() {
         super.onCreate();
